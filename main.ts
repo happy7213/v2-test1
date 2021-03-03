7d7ed51cb3f957dddb8b61e4b2fe71259f584f17
Shin Dongwook <happy7213@gmail.com>
2021-03-03 14:51:56 +0900

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,18 @@
 input.onButtonPressed(Button.A, function () {
     soundExpression.giggle.playUntilDone()
 })
-input.onGesture(Gesture.SixG, function () {
-    soundExpression.twinkle.playUntilDone()
-})
 input.onGesture(Gesture.Shake, function () {
     soundExpression.hello.playUntilDone()
+})
+input.onGesture(Gesture.EightG, function () {
+    soundExpression.twinkle.playUntilDone()
 })
 input.onButtonPressed(Button.B, function () {
     soundExpression.happy.playUntilDone()
 })
 music.startMelody(music.builtInMelody(Melodies.Entertainer), MelodyOptions.Once)
 basic.forever(function () {
-    if (input.lightLevel() < 100) {
+    if (input.lightLevel() < 50) {
         soundExpression.yawn.playUntilDone()
     }
 })
