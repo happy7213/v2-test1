58726657576ceb0ab96918ba2f6043c6306ef666
Shin Dongwook <happy7213@gmail.com>
2021-03-07 07:57:28 +0900

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
-soundExpression.soaring.play_until_done()
+def on_logo_pressed():
+    basic.show_number(randint(1, 6))
+    soundExpression.hello.play_until_done()
+input.on_logo_event(TouchButtonEvent.PRESSED, on_logo_pressed)
+
+def on_button_pressed_a():
+    soundExpression.giggle.play_until_done()
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_b():
+    soundExpression.happy.play_until_done()
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+music.start_melody(music.built_in_melody(Melodies.POWER_UP), MelodyOptions.ONCE)
 
 def on_forever():
-    pass
+    if input.light_level() < 30:
+        pass
+    else:
+        pass
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,17 +1,21 @@
 input.onButtonPressed(Button.A, function () {
-    soundExpression.giggle.playUntilDone()
-})
-input.onGesture(Gesture.Shake, function () {
     soundExpression.hello.playUntilDone()
+    led.plotBarGraph(
+    input.lightLevel(),
+    256
+    )
+})
+input.onGesture(Gesture.EightG, function () {
+    soundExpression.giggle.playUntilDone()
+    basic.showNumber(randint(1, 6))
+    basic.pause(2000)
+    basic.showIcon(IconNames.Happy)
 })
 input.onButtonPressed(Button.B, function () {
-    soundExpression.happy.playUntilDone()
+    soundExpression.hello.playUntilDone()
+    basic.showString("" + (input.temperature()))
 })
 music.startMelody(music.builtInMelody(Melodies.PowerUp), MelodyOptions.Once)
 basic.forever(function () {
-    if (input.lightLevel() < 30) {
-        basic.showNumber(randint(1, 6))
-        soundExpression.hello.playUntilDone()
-        basic.clearScreen()
-    }
+	
 })
